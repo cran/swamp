89886eb77684d0c9b3705b07d0820cffb7bb3183
Martin Lauss <martin.lauss@med.lu.se>
2011-01-26 00:00:00 +0000

    version 1.0

---
+++ R/quickadjust.zero.R
@@ -0,0 +1,21 @@
+quickadjust.zero <-
+function(g,batches){
+
+  if(class(g)!="matrix"){stop("g is not a matrix")}
+  if(class(batches)!="factor"){stop("batches is not a factor")}
+  if(length(levels(batches))<1.5){stop("batches has to be a factor with at least two levels")}
+  if(length(batches)!=ncol(g)){stop("batches has not the same length as ncol(g)")}
+  if (any(table(batches)<1.5)){stop("a level of batches has one or zero counts")}
+  
+  gafter<-g
+  gafter[]<-NA
+  for (i in 1:length(levels(batches))){
+  index<-which(batches==levels(batches)[i])
+  gb<-g[,index]
+  rowMedian<-apply(gb, 1, median, na.rm = T) # median centering not mean centering (more robust)
+  k1<-(gb-rowMedian)
+  gafter[,index]<-k1
+  }
+  return(gafter)
+  }
+
